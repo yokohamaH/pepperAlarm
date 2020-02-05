2c3a72580502daae4af49d89270f878cbe125b34
intern <intern@AGENDAs-MacBook-Air.local>
2020-02-05 10:30:55 +0900

    時間の表示

--- mezamashi/html/scripts/main.js
+++ mezamashi/html/scripts/main.js
@@ -1,20 +1,7 @@
-﻿$(function() {
+﻿$(function () {
   // 画像の表示/非表示を切り替える
-  $.subscribeToALMemoryEvent("buttonOn", function (data) {
-    var item = data[0];
-    var visFlag = data[1];
-    if (visFlag) {
-        $(item).css('visibility','visible');
-    } else {
-        $(item).css('visibility','hidden');
-    }
+  $.subscribeToALMemoryEvent("timeViw", function (data) {
+    var item = data;
+    document.write(item);
   });
-  
-  // 背景画像を切り替える
-  $.subscribeToALMemoryEvent("changeBack", function (data) {
-    var src = data;
-    //$('.image_back').attr('src','images/sample.png');
-    $('.image_back').attr('src','images/'+src);
-  });
-
 });
